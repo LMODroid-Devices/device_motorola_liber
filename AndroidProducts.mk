b035b4140b487f959224da45231459a2f3c9a35d
LMO GerritBot <gerrit@libremobileos.com>
2023-01-13 04:07:16 +0000

    Adapt to LMODroid by device-add script
    Change-Id: Iea8180b5f63ee86eda4858bf0d455ac2c730fe3b

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_liber.mk
+    $(LOCAL_DIR)/lmodroid_liber.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_liber-eng \
-    lineage_liber-user \
-    lineage_liber-userdebug
+    lmodroid_liber-eng \
+    lmodroid_liber-user \
+    lmodroid_liber-userdebug
